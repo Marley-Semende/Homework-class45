0be72fbec977443e9b2f2ecba9f3b96b57d668e5
Marley-Semende <marley.semende@gmail.com>
2023-10-27 03:03:06 +0200

    Upadated Week1 tests and pass.txt files

--- 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
+++ 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
@@ -17,11 +17,23 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 function rollDice() {
   const results = [];
 
-  // TODO: expand the chain to include five dice
+  // Expand the chain to include five dice
   return rollDie(1)
     .then((value) => {
       results.push(value);
       return rollDie(2);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDie(3); // Add rollDie(3)
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDie(4); // Add rollDie(4)
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDie(5); // Add rollDie(5)
     })
     .then((value) => {
       results.push(value);
@@ -34,6 +46,7 @@ function main() {
     .then((results) => console.log('Resolved!', results))
     .catch((error) => console.log('Rejected!', error.message));
 }
+
 
 // ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
