aef803602f2e0bdd80144af3601eb31985bb36e6
Marley-Semende <marley.semende@gmail.com>
2023-10-27 03:22:02 +0200

    Committing changes to index.js

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -21,7 +21,7 @@ const requestData = async (url) => {
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error(`HTTP error, status: ${response.status}`);
+      throw new Error(`HTTP error, status = ${response.status}`);
     }
     return await response.json();
   } catch (error) {
